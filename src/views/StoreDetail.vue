ee4b23ce0ccd7cffbe782975026f010d57240b5c
wanghongtao <1196354180@qq.com>
2020-05-16 00:15:27 +0800

    webwork8

--- src/views/StoreDetail.vue
+++ src/views/StoreDetail.vue
@@ -1,8 +1,378 @@
 <template>
-    
+    <div>
+        <div id="j-header">
+            <div id="header" class="tshop-psm-shop-header">
+                <div id="header" class="tshop-psm-shop-header-enterprise businessuser">
+                    <div id="header-content" class="header-box w-full header-enterprise-tb">
+                        <div class="logo-search">
+                            <div class="logos">
+                                <a href="/" class="tb-logo">
+                                    <img src="../assets/fzu.png" alt="">
+                                </a>
+                            </div>
+                            <div class="j-search tb-header-search search">
+                                <div class="search-panel">
+                                    <form action="#" class="j-searchform">
+                                        <div class="search-buttons">
+                                            <button class="search-button">搜淘宝</button>
+                                            <button class="search-button secondary">搜本店</button>
+                                        </div>
+                                        <div class="search-panel-fields">
+                                            <div class="search-combobox">
+                                                <div class="search-combobox-input-wrap">
+                                                    <input type="text">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="search-bottom">
+                                    <a href="">收藏店铺</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="shop-summary j-tshopsummary">
+                            <div class="shop-name">
+                                <div class="shop-name-wrap">
+                                    <a href="" class="shop-name-link"></a>
+                                </div>
+                                <div class="shop-rank-wrap">
+                                    <span class="shop-rank"></span>
+                                </div>
+                            </div>
+                            <div class="shop-service-info">
+                                <ul class="shop-service-info-list">
+                                    <li class="shop-service-info-item">
+                                        <span class="title">描述</span>
+                                        <span class="rateinfo">
+                                            <em>5.0</em>
+                                        </span>
+                                    </li>
+                                    <li class="shop-service-info-item">
+                                        <span class="title">服务</span>
+                                        <span class="rateinfo">
+                                            <em>5.0</em>
+                                        </span>
+                                    </li>
+                                    <li class="shop-service-info-item">
+                                        <span class="title">物流</span>
+                                        <span class="rateinfo">
+                                            <em>5.0</em>
+                                        </span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="page">
+            <div id="content">
+                <div id="bd">
+                    <div id="detail">
+                        <div class="tb-detail-bd tb-clear">
+                            <div class="tb-summary tb-clear">
+                                <div class="tb-item-info tb-clear">
+                                    <div class="tb-item-info-l">
+                                        <div class="tb-gallery">
+                                            <div class="tb-booth tb-pic tb-main-pic"></div>
+                                        </div>
+                                        <div class="tb-social tb-clearfix">
+                                            <ul>
+                                                <li class="tb-social-fav">
+                                                    <a href="">收藏宝贝</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="tb-item-info-r">
+                                        <div class="tb-property tb-property-x">
+                                            <div class="tb-wrap tb-wrap-newshop">
+                                                <div id="j-title" class="tb-title">
+                                                    <h3 class="tb-main-title">会恶化北京河北从胶合板厂我</h3>
+                                                    <p class="tb-subtitle"></p>
+                                                </div>
+                                                <ul class="tb-meta">
+                                                    <li id="j-strpricemodbox" class="tb-detail-price tb-clear j-priceitem">
+                                                        <span class="tb-property-type">价格</span>
+                                                        <div class="tb-property-cont">
+                                                            <strong id="j-strprice">
+                                                                <em class="tb-rmb">¥</em>
+                                                                <em class="tb-rmb-num">181.20-381.80</em>
+                                                            </strong>
+                                                        </div>
+                                                    </li>
+                                                    <li id="j-counter" class="tb-counter">
+                                                        <div class="tb-counter-bd">
+                                                            <div class="tb-rate-counter">
+                                                                <a href="">
+                                                                    <strong class="tb-zero">0</strong>
+                                                                    <span>累计评论</span>
+                                                                </a>
+                                                            </div>
+                                                            <div class="tb-sell-counter">
+                                                                <a href="">
+                                                                    <strong class="tb-zero">0</strong>
+                                                                    <span>交易成功</span>
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                                <div id="j-logistic">
+                                                    <div class="tb-logistic tb-clearfix">
+                                                        <span class="tb-name tb-property-type">配送</span>
+                                                    </div>
+                                                </div>
+                                                <div id="j-sepline" class="sep-line"></div>
+                                                <div id="j-isku" class="tb-key tb-key-sku">
+                                                    <div class="tb-skin">
+                                                        <dl class="j-prop tb-prop tb-clear" id="">
+                                                            <dt class="tb-property-type">颜色分类</dt>
+                                                            <dd>
+                                                                <ul class="j-tsaleprop tb-img tb-clearfix">
+                                                                    <li>
+                                                                        <a href="">
+                                                                            <img src="" alt="">
+                                                                        </a>
+                                                                    </li>
+                                                                </ul>
+                                                            </dd>
+                                                        </dl>
+                                                        <dl class="tb-amount tb-clear" id="">
+                                                            <dt class="tb-property-type">数量</dt>
+                                                            <dd>
+                                                                <span class="tb-stock" id="j-stock">
+                                                                    <button>-</button>
+                                                                    <span>1</span>
+                                                                    <button>+</button>
+                                                                    件
+                                                                </span>
+                                                                <em>
+                                                                    (库存
+                                                                    <span class="tb-count" id="j-spanstock">1542</span>
+                                                                    件)
+                                                                </em>
+                                                            </dd>
+                                                        </dl>
+                                                        <dl class="tb-clearfix" id="j-tbmultiterms">
+                                                            <dt class="tb-property-type">花呗分期</dt>
+                                                            <dd>
+                                                                <ul class="tb-clearfix j-tbmultiterms">
+                                                                    <li class="tb-multiterms-step tb-multiterms-step-enabled">
+                                                                        ￥31.55x6期<br>(含手续费)
+                                                                    </li>
+                                                                    <li class="tb-multiterms-step tb-multiterms-step-enabled">
+                                                                        ￥31.55x6期<br>(含手续费)
+                                                                    </li>
+                                                                    <li class="tb-multiterms-step tb-multiterms-step-enabled">
+                                                                        ￥31.55x6期<br>(含手续费)
+                                                                    </li>
+                                                                </ul>
+                                                            </dd>
+                                                        </dl>
+                                                        <div class="tb-action tb-clearfix" id="j-juvalid">
+                                                            <div class="tb-btn-buy">
+                                                                <a href="" class="j-linkbuy">立即购买</a>
+                                                            </div>
+                                                            <div class="tb-btn-add">
+                                                                <a href="" class="j-linkadd">
+                                                                    <i class="el-icon-shopping-cart-2"></i>
+                                                                    加入购物车
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="tb-extra" id="j-tbextra"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tb-sidebar tb-clear"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <style>
-
+#J_Header {
+    min-height: 70px;
+}
+.tshop-psm-shop-header {
+    position: relative;
+    z-index: 199;
+    min-width: 950px;
+    width: auto;
+    border-bottom: 1px solid #ccc;
+    background-color: #fff;
+}
+.tshop-psm-shop-header-enterprise {
+    position: relative;
+    z-index: 1999;
+    min-width: 950px;
+    width: auto;
+    border-bottom: 1px dotted #f40;
+    background-color: #FFF;
+}
+.tshop-psm-shop-header-enterprise .header-box {
+    position: relative;
+    width: 1140px;
+    height: 70px;
+    max-width: 1190px;
+    margin: 0 auto;
+    min-width: 950px;
+    color: #3c3c3c;
+}
+.tshop-psm-shop-header-enterprise .logo-search {
+    float: left;
+    height: 70px;
+    width: 130px;
+}
+.tshop-psm-shop-header-enterprise .logos {
+    margin-top: 12px;
+    width: 668px;
+    height: 44px;
+    float: left;
+}
+.tb-header-search {
+    position: absolute;
+    top: 12px;
+    right: 0;
+}
+.logos .tb-logo {
+    color: #f40;
+    width: 66px;
+    height: 44px;
+    word-wrap: break-word;
+    text-decoration: none!important;
+    float: left;
+}
+.logos .tb-logo img{
+    width: 66px;
+    height: 44px;
+}
+.search-panel {
+    width: 430px;
+    position: relative;
+}
+.search-panel .search-buttons {
+    position: relative;
+    z-index: 1;
+    float: right;
+    background-color: #fff;
+}
+.tshop-psm-shop-header-enterprise .search-button {
+    cursor: pointer;
+    border: 0;
+    font-size: 12px;
+    float: left;
+    width: 72px;
+    height: 30px;
+    background-color: #f40;
+    color: #fff;
+    margin-left: -1px;
+}
+.search-button.secondary {
+    background-color: #ffe4d0;
+    color: #f40;
+    border: 1px solid #ffb086;
+    margin-left: 3px;
+}
+.search-panel-fields {
+    height: 30px;
+    position: relative;
+    zoom: 1;
+    overflow: hidden;
+    background: #f40;
+}
+.search-combobox {
+    margin-left: 2px;
+    margin-right: 0;
+    margin-top: 2px;
+    height: 26px;
+    background: #fff;
+}
+.search-combobox-input-wrap {
+    position: relative;
+    overflow: hidden;
+    height: 100%;
+}
+.search-panel-fields input {
+    font-size: 14px;
+    position: relative;
+    height: 20px;
+    line-height: 20px;
+    padding: 3px;
+    margin: 2px 0 0 2px;
+    vertical-align: middle;
+    border: 0 none;
+    z-index: 2;
+    width: 100%;
+    outline: 0;
+    text-indent: 5px;
+}
+.search-bottom {
+    clear: both;
+    float: right;
+    text-align: right;
+    white-space: nowrap;
+    padding: 5px 0;
+    height: 16px;
+    line-height: 16px;
+    font-size: 12px;
+}
+.tshop-psm-shop-header-enterprise .shop-summary {
+    position: relative;
+    float: left;
+    width: 500px;
+    height: 70px;
+}
+.shop-summary .shop-name {
+    margin-top: 15px;
+    height: 40px;
+    padding: 0 10px;
+    border-left: 1px solid #f1f1f1;
+    float: left;
+}
+.shop-summary .shop-name-link {
+    font-weight: 600;
+    color: #333;
+}
+.tshop-psm-shop-header-enterprise .shop-service-info {
+    float: left;
+    margin-top: 15px;
+    height: 40px;
+    min-height: 40px;
+    width: 120px;
+    margin-left: 0;
+    padding: 0 0 0 10px;
+    border-left: 1px solid #f1f1f1;
+}
+.shop-service-info-list .shop-service-info-item {
+    width: 40px;
+    float: left;
+    text-align: left;
+}
+.shop-service-info-item .title {
+    display: block;
+    color: #333;
+    font-size: 12px;
+}
+.shop-service-info-item .rateinfo {
+    display: block;
+    color: #333;
+    font-size: 12px;
+}
+ .rateinfo em {
+    width: 24px;
+    float: left;
+    text-align: center;
+}
 </style>
 <script>
 export default {
